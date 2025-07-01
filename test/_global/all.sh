29a4e9a9ae97db4fb5a4f4d2b2bf446f8c6e7e29
Guistoff081 <guigolawliet13@gmail.com>
2025-07-01 18:35:53 -0300

    corrige os comandos de verificação para usar 'psql --version' em vez de 'postgis --version' nos testes de PostGIS

--- test/_global/all.sh
+++ test/_global/all.sh
@@ -18,7 +18,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "check for postgis" postgis --version
+check "check for postgis" psql --version
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
--- test/postgis/latest.sh
+++ test/postgis/latest.sh
@@ -10,7 +10,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "postgis latest version" postgis --version | grep 3.5
+check "postgis latest version" psql --version | grep 17
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
